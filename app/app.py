51de6fd1685bede6471286f2b147717166bae6ff
straighfacts07 <227547452+straightfacts07@users.noreply.github.com>
2026-06-17 11:23:04 +0530

    rewriting actual apis w.r.t our schema

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI ,HTTPException
+from fastapi import FastAPI ,HTTPException ,File , UploadFile, Depends,Form
 from app.schema import PostCreate , PostResponse
 from app.db import Post , create_db_and_tables , get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 
 @asynccontextmanager
@@ -14,47 +15,27 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-text_post = {
-    1: {"title": "post", "content": "cool post"},
-    2: {"title": "travel", "content": "visited a new city"},
-    3: {"title": "food", "content": "had delicious pizza"},
-    4: {"title": "coding", "content": "learned FastAPI today"},
-    5: {"title": "movie", "content": "watched a great film"},
-    6: {"title": "music", "content": "listening to new songs"},
-    7: {"title": "fitness", "content": "completed morning workout"},
-    8: {"title": "books", "content": "reading a mystery novel"},
-    9: {"title": "gaming", "content": "finished a tough level"},
-    10: {"title": "work", "content": "completed project milestone"}
-}
+@app.post("/upload")
+async def upload_file(
+    file:UploadFile=File(...),
+    caption:str=Form(...),
+    session:AsyncSession=Depends(get_async_session)  #dependency injection
+):
+    post=Post(
+        caption=caption,
+        url="dummyurl",
+        file_type="photo",
+        file_name="dummyfilename"
+    )
+    session.add(post)
+    await session.commit()  #adds the data to model
+    await session.refresh(post)
+    return post
 
-#limit is a list operation while text_post is a dictionary
-#we will use query parameters inside of a function
-@app.get("/posts")
-def get_all_posts(limit: int = None):
-    if limit:
-        return list(text_post.values())[:limit]
-    return text_post
-
-@app.get("/posts/{id}")
-def get_post(id:int)->PostResponse:
-    if id not in text_post:
-        raise HTTPException(status_code=404,detail="post not found")
-    return text_post.get(id)
-
-
-
-#we sent data from post api through a request body
-#we gonna create a schema for this
-#as we use pydantic model by default fastapi knows we are receiving a request body
-#fastapi automatically identifies the type of  the data sent
-#based on the data set in schema
-
-#we can only return data from the function which is the type of postresponse
-@app.post("/posts")
-def create_post(post:PostCreate)-> PostResponse:
-    new_post={"title":post.title,"content":post.content}
-    text_post[max(text_post.keys())+1] = new_post
-    return new_post
-     
-#output type= by default docs doesnt shows us much what type of data the api will return after a post request
-#so we can specify what type of data will be returned in the api itself
+@app.get("/feed")
+async def get_feed(
+    session:AsyncSession=Depends(get_async_session)
+):
+    result= await session.execute(select(Post).order_by(Post.created_At.desc()))
+    posts= [row[0] for row in result.all()]
+    
